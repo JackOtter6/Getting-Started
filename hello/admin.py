5686f75561eb42754daccd6d5d0d621fc84b1886
Liping Yi <LXY14@courses.opp.pennstate>
2020-07-08 20:31:55 -0400

    Admin

--- hello/admin.py
+++ hello/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Greeting, Game
+
+admin.site.register(Greeting)
+admin.site.register(Game)
--- hello/models.py
+++ hello/models.py
@@ -3,3 +3,9 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField("date created", auto_now_add=True)
+
+class Game(models.Model):
+    name = models.CharField(max_length=225)
+    information = models.TextField()
+    link = models.URLField()
+    
